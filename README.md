b29962a0eae10a2536a66caa2a46974d1cd28225
root <root@mqams-svr1.dev.fyre.ibm.com>
2025-11-06 23:45:27 -0500

    Added a simple instruction to create set-mqserver.sh script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ref: https://www.ibm.com/support/pages/protecting-message-data-implementing-mq-ams-end-end-message-security
 
+1. Create a file set-mqserver.sh
+   ```
+   export MQSERVER='DEV.APP.SVRCONN/TCP/yourhostname(1414)'
+   ```
+
--- put.sh
+++ put.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+. ./set-mqserver.sh
+
 QUEUE=${1:-CONFIDENTIALITY.Q}
 
 export PATH=$PATH:/opt/mqm/samp/bin
-export MQSERVER='DEV.APP.SVRCONN/TCP/mqams-svr1.dev.fyre.ibm.com(1414)'
 export MQSAMP_USER_ID=alice
 export MQS_KEYSTORE_CONF=~/.mqs/alice-keystore.conf
 amqsputc ${QUEUE} QM1
